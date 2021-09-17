268c4b56786c8d2a1ea5c85c9baa95873d91b67d
AriasCookie <90468069+AriasCookie@users.noreply.github.com>
2021-09-16 19:21:24 -0500

    bop
    bop

---
+++ src/calculadoraip/CalculadoraIP.java
@@ -0,0 +1,13 @@
+package calculadoraip;
+
+/**
+ *
+ * @author AriasUwU
+ */
+public class CalculadoraIP {
+    public static void main(String[] args) {
+       calculadora a = new calculadora();
+       a.setVisible(true);
+    }
+    
+}
